a36a544a5d2b79dea6dc4b377b46b9b4e8aa9bcc
Yasith Jayawardana <yasith.jayawardana@icloud.com>
2018-04-14 11:26:43 +0530

    cleanup code

--- core/src/main/java/com/grydtech/msstack/core/MicroserviceRunner.java
+++ core/src/main/java/com/grydtech/msstack/core/MicroserviceRunner.java
@@ -4,24 +4,25 @@ import java.util.Set;
 
 public final class MicroserviceRunner {
 
-    private MicroserviceRunner() {
-    }
+	private MicroserviceRunner() {
+	}
 
-    public static void run(Class<? extends MicroserviceApplication> applicationClass) throws Exception {
-        MicroserviceApplication microserviceApplication = applicationClass.newInstance();
+	public static void run(Class<? extends MicroserviceApplication> applicationClass) throws Exception {
+		MicroserviceApplication microserviceApplication = applicationClass.newInstance();
 
-        Set<Class<? extends GenericHandler>> handlers = microserviceApplication.getHandlers();
+		Set<Class<? extends GenericHandler>> handlers = microserviceApplication.getHandlers();
 
-        handlers.forEach(handler -> {
-            try {
-                if (handler.isAssignableFrom(EventHandler.class)) {
-                    MessageBroker.class.newInstance().registerHandler(handler.asSubclass(EventHandler.class));
-                }
-            } catch (InstantiationException | IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        });
+		handlers.forEach(handler -> {
+			try {
+				if (handler.isAssignableFrom(EventHandler.class)) {
+					MessageBroker.class.newInstance().registerHandler(handler.asSubclass(EventHandler.class));
+				}
+			}
+			catch (InstantiationException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		});
 
-        microserviceApplication.start();
-    }
+		microserviceApplication.start();
+	}
 }
--- microservices/jaxrs/src/main/java/com/grydtech/msstack/microservices/jaxrs/JaxRSApplication.java
+++ microservices/jaxrs/src/main/java/com/grydtech/msstack/microservices/jaxrs/JaxRSApplication.java
@@ -1,19 +1,19 @@
 package com.grydtech.msstack.microservices.jaxrs;
 
-import com.grydtech.msstack.core.MicroserviceApplication;
-import com.grydtech.msstack.microservices.jaxrs.features.JacksonFeature;
-
 import javax.ws.rs.Path;
 import java.util.HashSet;
 import java.util.Set;
+
+import com.grydtech.msstack.core.MicroserviceApplication;
+import com.grydtech.msstack.microservices.jaxrs.features.JacksonFeature;
 
 public abstract class JaxRSApplication extends MicroserviceApplication {
 
 	@Override
 	public Set<Class<?>> getClasses() {
 		Set<Class<?>> classes = new HashSet<>();
-		getHandlers().forEach(handlerClass-> {
-			if(handlerClass.isAnnotationPresent(Path.class)){
+		getHandlers().forEach(handlerClass -> {
+			if (handlerClass.isAnnotationPresent(Path.class)) {
 				classes.add(handlerClass);
 			}
 		});
