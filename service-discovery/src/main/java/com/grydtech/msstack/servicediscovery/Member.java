0928cf13e19ec53a6c6c25ff539361c22137201b
dilekamadushan <dileka.14@cse.mrt.ac.lk>
2018-05-06 18:38:20 +0530

    added loadbalancing feature

--- service-discovery/src/main/java/com/grydtech/msstack/servicediscovery/Member.java
+++ service-discovery/src/main/java/com/grydtech/msstack/servicediscovery/Member.java
@@ -4,33 +4,33 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 public class Member {
-
-    private final String name;
-
-    private final JsonObject data;
-
-    public Member(String name, JsonObject data) {
-        this.name = name;
-        this.data = data;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getIp() {
-        JsonElement val = data.get("address.ip");
-        return String.valueOf(val);
-    }
-
-    public int getPort() {
-        JsonElement val = data.get("address.port");
-        return Integer.parseInt(String.valueOf(val));
-    }
-
-    public String getId() {
-        JsonElement val = data.get("id");
-        return String.valueOf(val);
-    }
-
+	
+	private final String name;
+	
+	private final JsonObject data;
+	
+	public Member(String group, String name, JsonObject data) {
+		this.name = name;
+		this.data = data;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getIp() {
+		JsonElement val = data.get("address.ip");
+		return String.valueOf(val);
+	}
+	
+	public int getPort() {
+		JsonElement val = data.get("address.port");
+		return Integer.parseInt(String.valueOf(val));
+	}
+	
+	public String getId() {
+		JsonElement val = data.get("id");
+		return String.valueOf(val);
+	}
+	
 }
