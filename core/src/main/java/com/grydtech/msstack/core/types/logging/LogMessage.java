05ea601dcbe6be531b0ba20e337ca465658ccc87
Randil Fernando <randil.fernando@outlook.com>
2018-11-15 02:17:47 +0530

    log message structure changed

--- core/src/main/java/com/grydtech/msstack/core/types/logging/LogMessage.java
+++ core/src/main/java/com/grydtech/msstack/core/types/logging/LogMessage.java
@@ -1,17 +1,15 @@
 package com.grydtech.msstack.core.types.logging;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.grydtech.msstack.config.ConfigKey;
 import com.grydtech.msstack.config.ConfigurationProperties;
 import lombok.Data;
 
-import java.util.Date;
-
 @Data
 public class LogMessage {
-    private final String serviceGroup = ConfigurationProperties.get(ConfigKey.SERVICE_NAME);
-    private final String serviceId = ConfigurationProperties.get(ConfigKey.SERVICE_ID);
+    private final String name = ConfigurationProperties.get(ConfigKey.SERVICE_ID);
     private final String level;
-    private final Date date = new Date();
     private final String message;
+    @JsonProperty("class")
     private final String className;
 }
